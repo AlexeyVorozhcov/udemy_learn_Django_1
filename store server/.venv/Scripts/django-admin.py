5680c24adb87f5ed9e6a4c69389363678c3f9585
Alexey Vorozhtcov <alexey.vorozhcov@gmail.com>
2021-09-10 15:10:46 +0300

    init rep

---
+++ store server/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alexe\pycharmprojects\udemy jango 1\store server\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
